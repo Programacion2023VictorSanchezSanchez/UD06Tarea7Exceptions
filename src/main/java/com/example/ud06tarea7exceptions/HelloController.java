60434339beb1f6f051915448a7723481d074330d
vagosu <victorss_26@hotmail.com>
2024-03-21 16:21:52 +0100

    Inicio

---
+++ src/main/java/com/example/ud06tarea7exceptions/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.ud06tarea7exceptions;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.ud06tarea7exceptions {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.ud06tarea7exceptions to javafx.fxml;
+    exports com.example.ud06tarea7exceptions;
+}
